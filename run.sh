44e0979142d3b62a2d4dc81c33af2d34f930cee1
Ruslan Ibragimov <ibrmov.rusl@mail.ru>
2025-12-04 01:21:10 +0500

    feat: add local time sync between host system and container for correct script work in Lab2. Add comments

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 IMAGE_NAME="ais-labs"
 LABS_MOUNT_DIR="$(pwd)/Labs"
 
+# Build the Docker image from Dockerfile
 docker build -t "$IMAGE_NAME" .
 
-docker run -it --rm -v "$LABS_MOUNT_DIR":/Labs "$IMAGE_NAME" bash
+# Run container:
+# - Mount Labs directory
+# - Set local time inside container using host settings
+#     ro for read-only to avoid host's files modification
+# - Specify image and start bash shell
+docker run -it --rm \
+    -v "$LABS_MOUNT_DIR":/Labs \
+    -v /etc/localtime:/etc/localtime:ro \
+    "$IMAGE_NAME" bash
